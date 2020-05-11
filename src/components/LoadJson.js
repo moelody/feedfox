c448c56be83ad099b6d1f0b7d472458a4ba7e571
manikandanraji <manikandan4871113@gmail.com>
2020-05-12 01:19:24 +0530

    minor fix

--- src/components/LoadJson.js
+++ src/components/LoadJson.js
@@ -3,23 +3,27 @@ import styled from "styled-components";
 import { toast } from "react-toastify";
 import { LoadIcon } from "./Icons";
 
-const Wrapper = styled.a`
-  cursor: pointer;
-  display: flex;
-  align-items: center;
+const Wrapper = styled.h3`
+	cursor: pointer;
+	a {
+		display: flex;
+		align-items: center;
+	}
 
-  svg {
-    margin-right: 0.5rem;
-  }
+	svg {
+		margin-right: 0.5rem;
+	}
 `;
 
 const LoadJson = () => {
-  return (
-    <Wrapper href="https://drive.google.com/file/d/1io4cwvR35uoNI1qJF0ToueoiJFqwkZ6r/view">
-      <LoadIcon />
-      <span>Try Feed</span>
-    </Wrapper>
-  );
+	return (
+		<Wrapper>
+			<a href="https://drive.google.com/file/d/1io4cwvR35uoNI1qJF0ToueoiJFqwkZ6r/view">
+				<LoadIcon />
+				<span>Try Feed</span>
+			</a>
+		</Wrapper>
+	);
 };
 
 export default LoadJson;
